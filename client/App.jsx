19e090a52435ec585ccd77e4b1267a2b1bcb5b2c
Tye <tyemacon@gmail.com>
2019-10-11 16:18:15 -0500

    Fetches urls for given productId and renders first one to page

--- client/App.jsx
+++ client/App.jsx
@@ -1,16 +1,57 @@
 import React from 'React';
+import axios from 'axios';
+import Style from './App.css';
 
 class App extends React.Component {
   constructor(props){
     super(props);
-
+    this.state = {
+      test: 'Not Hello World',
+      url_75x75s: [],
+      url_170x135s: [],
+      url_570xNs: [],
+      url_fullxfull: [],
+    }
   }
 
+  getImageUrls(){
+    axios.get('http://localhost:3003/urls', {
+      params: {
+        productId: 729513146
+      }
+    })
+    .then((urls) => {
+      console.log(urls);
+      const seventyFives = [];
+      const oneSeventies = [];
+      const fiveSeventies = [];
+      const fulls = [];
+      let images = urls.data[0].Images
+      for(let i = 0; i < images.length; i++){
+        seventyFives.push(images[i].url_75x75);
+        oneSeventies.push(images[i].url_170x135);
+        fiveSeventies.push(images[i].url_570xN);
+        fulls.push(images[i].url_fullxfull);
+      }
+      this.setState({
+        url_75x75s: seventyFives,
+        url_170x135s: oneSeventies,
+        url_570xN: fiveSeventies,
+        url_fullxfull: fulls,
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
 
   render(){
     return (
       <div>
-        <h1>This Should Render on the page</h1>
+        <h1>{this.state.test}</h1>
+        <img src={this.state.url_170x135s[0]}/>
+        <h3 className={Style.test}>One more</h3>
+        <button onClick={this.getImageUrls.bind(this)}>Get Urls</button>
       </div>
     )
   }
--- server/server.js
+++ server/server.js
@@ -7,10 +7,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/../dist')));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
-
-module.exports.port = port;
