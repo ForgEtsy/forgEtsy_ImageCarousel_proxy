3841848cbfcee12c869cfdbc30c0506f4f4c7014
Tye <tyemacon@gmail.com>
2019-10-17 13:58:51 -0500

    arrows are mostly equal

--- client/components/MainImage/MainImage.jsx
+++ client/components/MainImage/MainImage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import Style from './MainImage.css'
+import Style from './MainImage.css';
+import Arrow from '../Arrow/Arrow.jsx';
 
-const MainImage = ({ url }) => {
+const MainImage = ({ url, scrollLeft, scrollRight }) => {
     return (
         <div className={Style.mainimage}>
+            {<Arrow direction={1} onclick={scrollLeft} char="&#9664;"/>}
+            {<Arrow direction={0} onclick={scrollRight} char="&#9654;"/>}
             <img src={url}/>
         </div>
     )
