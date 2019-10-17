41cbb26e39e8c51e98f78258dcfe5917414c97f2
Tye <tyemacon@gmail.com>
2019-10-17 15:05:30 -0500

    much prettier arrows

--- client/components/MainImage/MainImage.jsx
+++ client/components/MainImage/MainImage.jsx
@@ -5,8 +5,8 @@ import Arrow from '../Arrow/Arrow.jsx';
 const MainImage = ({ url, scrollLeft, scrollRight }) => {
     return (
         <div className={Style.mainimage}>
-            {<Arrow direction={1} onclick={scrollLeft} char="&#9664;"/>}
-            {<Arrow direction={0} onclick={scrollRight} char="&#9654;"/>}
+            {<Arrow direction={1} onclick={scrollLeft} char="&#8592;"/>}
+            {<Arrow direction={0} onclick={scrollRight} char="&#8594;"/>}
             <img src={url}/>
         </div>
     )
