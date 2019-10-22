90fd43da4b2b85bece6b67e0205dad5267181861
Tye <tyemacon@gmail.com>
2019-10-21 19:57:08 -0500

    file purge

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/../dist')));
+app.use(cors());
 
 const port = 3000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + '/../dist/index.html'));
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
